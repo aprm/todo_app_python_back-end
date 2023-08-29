cd181d428598da9a1618582fd4c5e176bc0a07bb
aprm <pal.wrk@gmail.com>
2023-08-29 19:58:03 +0300

    add list-items functionality and unit tests

--- tests/unit/views/test_default_view.py
+++ tests/unit/views/test_default_view.py
@@ -5,6 +5,39 @@ from todo_app_python_back_end.views.default import get_items, add_item
 from tests.utils.helpers import get_mock_request, MockDbSession
 
 
+def test_get_items():
+    """
+    Test that the get_items view returns an empty list when there are no items in the database.
+    """
+    request = get_mock_request()
+    response = get_items(request)
+    assert response['todolist'] == []
+
+
+def test_get_items_with_items():
+    """
+    Test that the get_items view returns a list of items when there are items in the database.
+    """
+    request = get_mock_request()
+    # add a mock item to the database directly
+    mock_item = Mock(name='test item')
+    request.dbsession.items.append(mock_item)
+
+    response = get_items(request)
+    assert response['todolist'][0] == mock_item
+
+
+def test_get_items_with_db_error():
+    """
+    Test that the get_items view returns a 500 response when there is an error with the database.
+    """
+    with patch.object(MockDbSession, 'query', side_effect=SQLAlchemyError):
+        request = get_mock_request()
+        response = get_items(request)
+        assert response.status_code == 500
+        assert response.text == 'Internal Error'
+
+
 def test_add_item():
     """
     Test that the add_item view adds a new item to the database.
@@ -12,7 +45,11 @@ def test_add_item():
     request = get_mock_request(post_params={'name': 'test item'})
     response = add_item(request)
 
-    assert response['todolist'][0].name == 'test item'
+    assert response.status_code == 301
+    assert response.text == 'Task added'
+
+    # check that the item was added to the database
+    assert request.dbsession.items[0].name == 'test item'
 
 
 def test_add_item_without_name():
--- todo_app_python_back_end/views/default.py
+++ todo_app_python_back_end/views/default.py
@@ -4,7 +4,6 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from .. import models
 
-
 @view_config(route_name='home', renderer='todo_app_python_back_end:templates/home.jinja2')
 def get_items(request):
     try:
@@ -21,16 +20,11 @@ def add_item(request):
     try:
         name = request.POST.get('name')
         if not name:
-            return Response("Task name should not be empty!", content_type='text/plain', status=400)    
+            return Response("Task name should not be empty!", content_type='text/plain', status=400)
         new_item = models.TodoItem()
         new_item.name = name
         request.dbsession.add(new_item)
         request.dbsession.flush()
     except SQLAlchemyError:
         return Response("Internal Error", content_type='text/plain', status=500)
-    return {
-        'todolist': request.dbsession.query(models.TodoItem).all()
-    }
-
-
-
+    return Response("Task added", content_type='text/plain', status=301, location='/')
